f3b57b750afc11bf07820b7dcc0286005dfc0035
dmcginley <mcginley.donn@gmail.com>
2022-04-18 22:34:39 +0100

    added grid for game and imported to run.py

--- run.py
+++ run.py
@@ -1,12 +1,21 @@
-board = []
+#import grid
 
-for i in range(5):
-    board.append(["O"] * 5)
-
-
-def print_board(board_in):
-    for row in board:
-        print(" ".join(row))
+from grid import Grid
 
 
-print_board(board)
+player_a_ship_grid = Grid(4)
+player_a_ship_grid.print_board()
+
+
+# board = []
+#
+# for i in range(5):
+#     board.append(["O"] * 5)
+#
+#
+# def print_board(board_in):
+#     for row in board:
+#         print(" ".join(row))
+#
+#
+# print_board(board)
