38ff61303017f251bd3ea6ec1e772cd59d6b2ed1
dmcginley <mcginley.donn@gmail.com>
2022-04-19 23:17:48 +0100

    feat: changed method for alphabet and add todo note to grid.py

--- grid.py
+++ grid.py
@@ -1,27 +1,30 @@
 class Grid:
     def __init__(self, grid_size):
         print('creating new Grid object here')
+
+        # TODO: max size 10, min size 2 for grid
         self.grid_data = []
         for i in range(grid_size):
             row = ["~"] * grid_size
             self.grid_data.append(row)
 
     def print_board(self):
-        row_letters = ['A', 'B', 'C', 'D']
+        # row_letters = ['A', 'B', 'C', 'D']
 
-        #r = 0
-        #for row in self.grid_data:
-        #for r in range(len(self.grid_data)):
-        grid_width = len(self.grid_data) # grid is always square
+        row_letters = 'ABCDEFGHIJ'
+
+        # r = 0
+        # for row in self.grid_data:
+        # for r in range(len(self.grid_data)):
+
+        grid_width = len(self.grid_data)  # grid is always square
         col_markers = []
         for col in range(grid_width):
-            col_markers.append(str(col+1)) # grid counts from 1
+            col_markers.append(str(col+1))  # grid counts from 1
         print("  " + " ".join(col_markers))
 
         for r, row in enumerate(self.grid_data):
             row = self.grid_data[r]
             row_cells = " ".join(row)
             print(f"{row_letters[r]} {row_cells}")
-            #r = r+1
-
-
+            # r = r+1
